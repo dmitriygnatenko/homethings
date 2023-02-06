ecd2f78d1611f02ad5892d9ae36ac5d08265248c
Dima <info@dmitriygnatenko.ru>
2023-02-06 21:40:10 +0200

    [backend] Add initial user

---
+++ migrations/20221115102500_add_user.sql
@@ -0,0 +1,6 @@
+-- +goose Up
+-- admin/admin
+INSERT INTO "user" (username, password) VALUES ('admin', '$2a$10$kSMPeUITJB9tZ2bs.Wm5gukp0Vv9qP3P.K.To.S2OrqAQv0mhzgtS');
+
+-- +goose Down
+DELETE FROM "user" WHERE username='admin';
