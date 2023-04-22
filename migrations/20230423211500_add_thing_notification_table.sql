6d6fda5961f754cb8e7b55a7418e7297b9806673
Dima <info@dmitriygnatenko.ru>
2023-04-22 22:03:59 +0200

    [backend] Add notification migration

---
+++ migrations/20230423211500_add_thing_notification_table.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE thing_notification
+(
+    thing_id          INT PRIMARY KEY,
+    notification_date TIMESTAMP NOT NULL,
+    created_at        TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at        TIMESTAMP NOT NULL DEFAULT NOW(),
+    CONSTRAINT fk_thing_notification_thing_id FOREIGN KEY (thing_id) REFERENCES thing (id)
+);
+
+-- +goose Down
+DROP TABLE thing_notification;
